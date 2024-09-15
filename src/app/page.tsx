e512b6892a7ca64cab4e30c1d4b0573eaeb306af
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-09-15 18:20:14 +0600

    dash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import Header from "@/components/header/header";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Link from "next/link";
+import { ComutiryBanner } from '@/components/comunityBanner/page';
+import { Content } from '@/components/content/page';
+import Header from '@/components/header/header';
+import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Content />
     </div>
   );
 }
---
+++ src/components/comunityBanner/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+
+interface Props {
+  className?: string;
+}
+
+export const ComutiryBanner: React.FC<Props> = ({ className }) => {
+  return (
+    <div className="bg-black text-white rounded-lg p-8 flex justify-between items-center max-w-5xl mx-auto">
+      {/* Левая часть: Текст и иконка */}
+      <div className="flex items-center space-x-4">
+        {/* Иконка Discord */}
+        <Image src="/icon/discord.svg" alt="" width={56} height={56} />
+        {/* Текст */}
+        <div>
+          <h2 className="text-2xl font-bold">Общайтесь с сообществом!</h2>
+          <p className="text-gray-400">
+            Не стесняйтесь задавать вопросы, сообщать о проблемах и знакомиться с новыми людьми.
+          </p>
+        </div>
+      </div>
+
+      {/* Кнопка */}
+      <Button className="bg-black rounded-3xl p-6 border">Join the #Leitdev Community!</Button>
+    </div>
+  );
+};
